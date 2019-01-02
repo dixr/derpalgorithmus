20fc5d88243a2b4f94dde6fbd19a90bd1befea64
Dennis & Dina <dennisdina@mail.de>
2019-01-02 19:13:17 +0100

    Changed class structure to one GUI class instead of several screens

--- main.py
+++ main.py
@@ -3,33 +3,45 @@ kivy.require('1.10.1')
 
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.properties import ObjectProperty
+from kivy.core.window import Window
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.actionbar import ActionBar, ActionView, ActionButton
-from kivy.uix.screenmanager import ScreenManager, Screen, RiseInTransition, FadeTransition
+from kivy.uix.screenmanager import ScreenManager, Screen, RiseInTransition, FadeTransition, SlideTransition
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 
-Builder.load_file('derpalgorithmus.kv')
+class DerPAlgorithmusGUI(BoxLayout):
+    sm = ObjectProperty(None)
 
-class PersonsScreen(Screen):
-    pass
+    def __init__(self, **kwargs):
+        super(DerPAlgorithmusGUI, self).__init__(**kwargs)
+        Window.bind(on_keyboard=self.key_handler)
 
-class ConditionsScreen(Screen):
-    pass
+    def key_handler(self, instance, key, *largs):
+        if key == 27: # back button in Android is treated as Esc
+            self.set_previous_screen()
+            return True
 
-class ResultsScreen(Screen):
-    pass
+    def set_previous_screen(self):
+        self.sm.transition = SlideTransition(direction='right')
+        if self.sm.current == 'Results':
+            self.sm.current = 'Conditions'
+        elif self.sm.current == 'Conditions':
+            self.sm.current = 'Persons'
 
-sm = ScreenManager(transition=FadeTransition())
-sm.add_widget(PersonsScreen(name='persons'))
-sm.add_widget(ConditionsScreen(name='conditions'))
-sm.add_widget(ResultsScreen(name='results'))
+    def set_next_screen(self):
+        self.sm.transition = SlideTransition(direction='left')
+        if self.sm.current == 'Persons':
+            self.sm.current = 'Conditions'
+        elif self.sm.current == 'Conditions':
+            self.sm.current = 'Results'
 
 class DerPAlgorithmus(App):
 
     def build(self):
-        return sm
+        return DerPAlgorithmusGUI()
 
 
 if __name__ == '__main__':
